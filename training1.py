d742cd129350c56e0a31b1c70bed471bbd8c894c
FlorianHeck2 <florianheck@mein.gmx>
2023-12-07 19:52:22 +0100

    First training completed

---
+++ training1.py
@@ -0,0 +1,8 @@
+if 5 != 3:
+    print("Five isn't three")
+if 5 < 3:
+    print("Five is smaller than three")
+if 5 > 3:
+    print("Five is smaller than three")
+if 5 == 3:
+    print("Five equals three")
