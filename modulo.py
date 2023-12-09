691f2ab15b0be332c0f49631040c2c056db2b457
FlorianHeck2 <florianheck@mein.gmx>
2023-12-09 12:29:40 +0100

    Modulo

---
+++ modulo.py
@@ -0,0 +1,6 @@
+print("This operation is supposed to give you an Modulo!")
+n1 = input("Enter the first Number: ")
+n2 = input("Enter a second Number: ")
+while( int(n1)- int(n2) >= 0):
+    n1 = int(n1) - int(n2)
+print(n1)
