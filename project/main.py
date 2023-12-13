2381be66b9264348fb3320d2cd0f7f661b582ce5
FlorianHeck2 <florianheck@mein.gmx>
2023-12-13 22:31:46 +0100

    working on movement

--- project/main.py
+++ project/main.py
@@ -19,39 +19,31 @@ class spieler:
         self.schritteRechts = schritteRechts
         self.last = [1,0]
         self.ok = True  
-    def laufen(self, Liste):
+    def laufen(self, Liste, geschw):
         if Liste[0]:
-             self.x -= self.geschw
+             self.x -= geschw
              self.richtg = [1,0,0,0]
              self.schritteLinks += 1
         elif Liste[1]:
-             self.x += self.geschw
+             self.x += geschw
              self.richtg = [0,1,0,0]
              self.schritteRechts += 1
         elif Liste[2]:
-             self.y -= self.geschw
+             self.y -= geschw
         elif Liste[3]:
-             self.y += self.geschw
-        
-    def Laufen(self):
-          self.x += self.geschw
-          if self.geschw > 0:
-               self.richtg = [0,1]
-               self.schritteRechts +=1
-          else:
-               self.richtg = [1,0]
-               self.schritteLinks += 1  
+             self.y += geschw
+
 
 screen = pygame.display.set_mode([1290,717])#Erzeugt Fenster mit Höhe und Breite in Pixeln
 clock = pygame.time.Clock()
-spieler1 = spieler(300,391,4,42,46,[0,0,0,0,1],0,0)
+spieler1 = spieler(100,191,4,42,46,[0,0,0,0,1],0,0)
 
 linkeWand = pygame.draw.rect(screen, (0,0,0), (0,0,2,717), 0)
 rechteWand = pygame.draw.rect(screen, (0,0,0), (1278,0,2,717), 0)
 barrier1 = pygame.draw.rect(screen, (0,0,0),(800, 112, 77, 456), 0)
 barrier2 = pygame.draw.rect(screen, (0,0,0),(0, 397, 677, 444), 0)
 Walls = [rechteWand, linkeWand, barrier1, barrier2]
-
+richtung = [1,0,0,0]
 Go = True
 
 while Go:
@@ -64,18 +56,29 @@ while Go:
     gedrueckt = pygame.key.get_pressed()
     for event in pygame.event.get():#Tastatur/Spielefenstereingaben abgreifen
         if event.type ==pygame.QUIT: sys.exit()#Spiel schließen
-    for wall in Walls:  
+    counter = 0
+    laengelist = 0
+    for wall in Walls: 
+        laengelist += 1 
         if not spielerrechteck.colliderect(wall):
+             counter += 1
+        if counter == 4:
             if gedrueckt[pygame.K_RIGHT] and not spielerrechteck.colliderect(rechteWand):# and not spielerRechteck.colliderect(rechteWand):#Pfeiltaste rechts
-                spieler1.laufen([0,1,0,0])
+                spieler1.laufen([0,1,0,0], spieler1.geschw)
+                richtung = [0,1,0,0]
             elif gedrueckt[pygame.K_LEFT]: #and not spielerRechteck.colliderect(linkeWand):#Pfeiltaste links
-                spieler1.laufen([1,0,0,0])
+                spieler1.laufen([1,0,0,0], spieler1.geschw)
+                richtung = [1,0,0,0]
             elif gedrueckt[pygame.K_UP]:
-                spieler1.laufen([0,0,1,0])
+                spieler1.laufen([0,0,1,0], spieler1.geschw)
+                richtung = [0,0,1,0]
             elif gedrueckt[pygame.K_DOWN]:
-                spieler1.laufen([0,0,0,1])
+                spieler1.laufen([0,0,0,1], spieler1.geschw)
+                richtung = [0,0,0,1]
+        else:
+            spieler1.laufen(richtung, -1* (spieler1.geschw))
 
     
-    richtung = [0,0,0,0,1] #Links,Rechts, Hoch, Runter, Stehen
-    clock.tick(20)
+    #richtung = [0,0,0,0,1] Links,Rechts, Hoch, Runter, Stehen
+    clock.tick(30)
     pygame.display.update()
