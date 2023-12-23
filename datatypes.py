4837e11ddf621a6a543f579d70209c83665cc76c
FlorianHeck2 <florianheck@mein.gmx>
2023-12-23 23:36:30 +0100

    experimenting with datatypes

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+a = range(6)
+b = range(7, 11, 1)
+c = b
+for n in range(4):
+    print(c[n])
+
+print(3**2)
+print("Hello"+" World")
+testmenge = {"a", "b"}
+e = testmenge - testmenge
+print(testmenge)
+print("--<-<-<@")
+
+l = [1, 'a', 5,1,2,5]
+z = set(l)
+print(z)
