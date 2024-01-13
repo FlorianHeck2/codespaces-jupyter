11cc0951d0ab74d016bdba648bd9464afe079f59
FlorianHeck2 <florianheck@mein.gmx>
2024-01-13 23:07:53 +0100

    update wall2

--- project/main.py
+++ project/main.py
@@ -49,7 +49,7 @@ class spieler:
 
 screen = pygame.display.set_mode([1290,717])#Erzeugt Fenster mit Höhe und Breite in Pixeln
 clock = pygame.time.Clock()
-spieler1 = spieler(100,191,4,42,46,[0,0,0,0,1],0,0)
+spieler1 = spieler(10,350,4,42,46,[0,0,0,0,1],0,0)
 
 linkeWand = pygame.draw.rect(screen, (0,0,0), (0,0,2,717), 0)
 obereWand = pygame.draw.rect(screen, (0,0,0), (0,0,1278,1),0)
@@ -57,13 +57,14 @@ rechteWand = pygame.draw.rect(screen, (0,0,0), (1278,0,2,717), 0)
 untereWand = pygame.draw.rect(screen, (0,0,0), (0,716,1278,717),0)
 barrier1 = pygame.draw.rect(screen, (0,0,0),(800, 112, 77, 456), 0)
 barrier2 = pygame.draw.rect(screen, (0,0,0),(0, 397, 677, 82), 0)
-Walls = [rechteWand, linkeWand, obereWand, barrier1, barrier2, untereWand]
+barrier3 = pygame.draw.rect(screen, (100,100,100),(0, 250, 677, 82), 0)
+Walls = [rechteWand, linkeWand, obereWand, barrier1, barrier2, barrier3, untereWand]
 richtung = [1,0,0,0]
 Go = True
-savex = 42
-savey = 46
-save2x = 42
-save2y = 46
+savex = 10
+savey = 350
+save2x = 10
+save2y = 350
 while Go:
     move = True
     spielerrechteck = pygame.Rect(spieler1.x, spieler1.y, 42, 46) 
@@ -72,8 +73,6 @@ while Go:
     for event in pygame.event.get():#Tastatur/Spielefenstereingaben abgreifen
         if event.type ==pygame.QUIT: sys.exit()#Spiel schließen
     counter = 0
-
-
     
     
     if gedrueckt[pygame.K_RIGHT]:# and not spielerRechteck.colliderect(rechteWand):#Pfeiltaste rechts
@@ -101,7 +100,6 @@ while Go:
     
     
     screen.blit(linksgehen, (spieler1.x,spieler1.y))    
-    screen.blit(Fackelschein, (spieler1.x-1900, spieler1.y-1050))
     if savex != spieler1.x or savey != spieler1.y:
         save2x = savex
         save2y = savey
@@ -110,4 +108,6 @@ while Go:
         savey = spieler1.y
     #richtung = [0,0,0,0,1] Links,Rechts, Hoch, Runter, Stehen
     clock.tick(30)
+    barrier4 = pygame.draw.rect(screen, (100,100,100),(0, 250, 677, 82), 0)    
+    screen.blit(Fackelschein, (spieler1.x-1900, spieler1.y-1050))
     pygame.display.update()
