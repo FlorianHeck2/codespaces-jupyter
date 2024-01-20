6e6fdb3f0eb357a1ef771d738ab0d441aaab010b
FlorianHeck2 <florianheck@mein.gmx>
2024-01-20 23:19:53 +0100

    doors

--- project/main.py
+++ project/main.py
@@ -6,6 +6,7 @@ hochgehen = pygame.image.load("project/Grafiken/Mainbild.png")
 runtergehen = pygame.image.load("project/Grafiken/Mainbild.png")
 Fackelschein = pygame.image.load("project/Grafiken/blurr.png")
 hintergrund = pygame.image.load("project/Grafiken/background.png")
+Tuere = False
 
 class spieler:
     def __init__(self,x,y,geschw,breite,hoehe,richtg,schritteLinks,schritteRechts):
@@ -45,6 +46,14 @@ class spieler:
              self.y -= self.geschw
         elif Liste[2]:
              self.y += self.geschw
+def zeichnen():
+    if Tuere == False:  
+        door = pygame.draw.rect(screen, (100,100,100),(657, 332, 20, 66), 0) 
+       
+    else:  
+        door = pygame.draw.rect(screen, (100,100,100),(657, 332, 66, 5), 0)
+        
+    screen.blit(Fackelschein, (spieler1.x-1900, spieler1.y-1050))
 
 
 screen = pygame.display.set_mode([1290,717])#Erzeugt Fenster mit Höhe und Breite in Pixeln
@@ -58,13 +67,16 @@ untereWand = pygame.draw.rect(screen, (0,0,0), (0,716,1278,717),0)
 barrier1 = pygame.draw.rect(screen, (0,0,0),(800, 112, 77, 456), 0)
 barrier2 = pygame.draw.rect(screen, (0,0,0),(0, 397, 677, 82), 0)
 barrier3 = pygame.draw.rect(screen, (100,100,100),(0, 250, 677, 82), 0)
-Walls = [rechteWand, linkeWand, obereWand, barrier1, barrier2, barrier3, untereWand]
+door = pygame.draw.rect(screen, (100,100,100),(657, 332, 20, 66), 0)
+Walls = [rechteWand, linkeWand, obereWand, barrier1, barrier2, barrier3, untereWand, door]
 richtung = [1,0,0,0]
 Go = True
 savex = 10
 savey = 350
 save2x = 10
 save2y = 350
+timer = 0
+doorcount = 0
 while Go:
     move = True
     spielerrechteck = pygame.Rect(spieler1.x, spieler1.y, 42, 46) 
@@ -73,7 +85,16 @@ while Go:
     for event in pygame.event.get():#Tastatur/Spielefenstereingaben abgreifen
         if event.type ==pygame.QUIT: sys.exit()#Spiel schließen
     counter = 0
-    
+    if gedrueckt[pygame.K_e]:
+        if doorcount <= 0:
+            if door in Walls: Walls.remove(door)
+            if Tuere == False:
+                Tuere = True
+            else: 
+                Tuere = False
+                Walls.append(door)
+            doorcount = 20
+    doorcount -= 1 #Zählvariable, um "Türspammen" zu verhindern
     
     if gedrueckt[pygame.K_RIGHT]:# and not spielerRechteck.colliderect(rechteWand):#Pfeiltaste rechts
         spieler1.laufen([0,1,0,0], spieler1.geschw)
@@ -108,6 +129,8 @@ while Go:
         savey = spieler1.y
     #richtung = [0,0,0,0,1] Links,Rechts, Hoch, Runter, Stehen
     clock.tick(30)
-    barrier4 = pygame.draw.rect(screen, (100,100,100),(0, 250, 677, 82), 0)    
-    screen.blit(Fackelschein, (spieler1.x-1900, spieler1.y-1050))
+    barrier4 = pygame.draw.rect(screen, (200,200,100),(0, 250, 677, 82), 0)   
+    zeichnen() 
+     
+
     pygame.display.update()
