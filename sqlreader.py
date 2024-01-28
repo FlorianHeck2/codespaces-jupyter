39e17e881b0962cf7821f3867588d53c65d33142
FlorianHeck2 <florianheck@mein.gmx>
2024-01-28 19:56:28 +0100

    further sql testing

--- sqlreader.py
+++ sqlreader.py
@@ -1,14 +1,26 @@
 import sqlite3
-connection = sqlite3.connect("company.db")
+con = sqlite3.connect("tutorial.db")
 
-cursor = connection.cursor()
+cur = con.cursor()
 
-cursor.execute("SELECT * FROM employee") 
-print("fetchall:")
-result = cursor.fetchall() 
-for r in result:
-    print(r)
-cursor.execute("SELECT * FROM employee") 
-print("\nfetch one:")
-res = cursor.fetchone() 
-print(res)
+
+# data = [
+#     ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+#     ("Monty Python's The Meaning of Life", 1983, 7.5),
+#     ("Monty Python's Life of Brian", 1979, 8.0),
+# ]
+# cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+# con.commit()
+
+#cur.execute("CREATE TABLE movie(title, year, score)")
+# cur.execute("""
+#     INSERT INTO movie VALUES
+#         ('Monty Python and the Holy Grail', 1975, 8.2),
+#         ('And Now for Something Completely Different', 1971, 7.5)
+# """)
+# res = cur.execute("SELECT name FROM sqlite_master")
+# res.fetchone()
+# ('movie',)
+# con.commit
+for row in cur.execute("SELECT year, title FROM movie ORDER BY year"):
+    print(row)
